f4199ff4abad900340b7b98bd28c4719e1eec229
tenzin333 <tenthinlay007@gmail.com>
2026-01-17 05:10:26 +0000

    security: automated vulnerability remediation

--- server/index.js
+++ server/index.js
@@ -1,28 +1,49 @@
-const http = require('http');
+const http = require('https');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const fs = require('fs');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const escapeRegExp = (s) => s.replace(/[.*+?^${}()|[\\]\\]/g, '\\\\$&');
 
+const app = express();
+const csrfProtection = csrf();
+
+app.disable('x-powered-by');
+app.use(helmet());
+app.use(express.json({ limit: '1mb' }));
+app.use(csrfProtection);
 app.use(cors());
+
 app.use(router);
+
+const server = http.createServer({
+  key: fs.readFileSync(process.env.TLS_KEY || 'TODO: Load TLS key'),
+  cert: fs.readFileSync(process.env.TLS_CERT || 'TODO: Load TLS cert'),
+}, app);
+
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if(error) return callback(error);
+    if (error) return callback(error);
 
     socket.join(user.room);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${escapeRegExp(user.room)}.`);
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
@@ -33,7 +54,7 @@ io.on('connect', (socket) => {
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('message', { user: user.name, text: escapeRegExp(message) });
 
     callback();
   });
@@ -41,11 +62,11 @@ io.on('connect', (socket) => {
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
 
-    if(user) {
+    if (user) {
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     }
-  })
+  });
 });
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
