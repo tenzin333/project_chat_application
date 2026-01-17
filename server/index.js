31bedfa3d30e47b36bef387de409d60edef8b5e0
tenzin333 <tenthinlay007@gmail.com>
2026-01-17 05:20:53 +0000

    security: automated vulnerability remediation

--- server/index.js
+++ server/index.js
@@ -1,22 +1,40 @@
-const http = require('http');
+const http = require('https');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const fs = require('fs');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const escapeRegExp = (s) => s.replace(/[.*+?^${}()|[\\]\\]/g, '\\\\$&');
 
+const app = express();
+const csrfProtection = csrf();
+
+app.disable("x-powered-by");
+app.use(helmet());
+app.use(express.json({ limit: "1mb" }));
+app.use(csrfProtection);
 app.use(cors());
+
 app.use(router);
+
+const server = http.createServer({
+  key: fs.readFileSync(process.env.TLS_KEY || 'path/to/tls/key'),
+  cert: fs.readFileSync(process.env.TLS_CERT || 'path/to/tls/cert'),
+}, app);
+
+const io = socketio(server);
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
+    if (!name || !room) return callback('Please provide a name and room');
+
+    const { error, user } = addUser({ id: socket.id, name, room: escapeRegExp(room) });
 
     if(error) return callback(error);
 
@@ -31,6 +49,8 @@ io.on('connect', (socket) => {
   });
 
   socket.on('sendMessage', (message, callback) => {
+    if (!message) return callback('Please provide a message');
+
     const user = getUser(socket.id);
 
     io.to(user.room).emit('message', { user: user.name, text: message });
